1bda6034dcd5fa3c3df313b870bd18c44314c4b0
SJ <bash-Queko@protonmail.com>
2021-03-30 19:21:56 +0100

    Add files via upload

---
+++ src/porter.h
@@ -0,0 +1,23 @@
+/*************************************************************************************************
+
+
+- PORTER.H -
+- 'NOISE' -
+
+Author - <SJ> || <bash-Queko@protonmail.com>
+Github - https://github.com/bash-Queko
+* New Github
+=======================================================
+Nested Object Inverted Search Engine
+- C++, Rust, C, Cmake -
+=======================================================
+
+
+***************************************************************************************************/
+
+#ifndef porter_h
+#define porter_h
+
+extern "C" int porter_stem_inplace(char * b, int k);
+
+#endif /* porter_h */
